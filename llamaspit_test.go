22d2afeaf5e2c8d71b94c91ef90909604af5f4f9
Ben Plunkert <ben@plunkert.com>
2024-01-30 16:35:52 -0500

    add parallel probabalistic testing

--- llamaspit_test.go
+++ llamaspit_test.go
@@ -4,22 +4,38 @@ import (
 	"bytes"
 	"os/exec"
 	"strings"
+	"sync"
 	"testing"
 )
 
 func TestProcessChatResponse(t *testing.T) {
-	t.Run("Test command execution on '-y' argument", func(t *testing.T) {
-		cmd := exec.Command("./llamaspit", "-y", "multiply 37 by 73")
-		var out bytes.Buffer
-		cmd.Stdout = &out
+	var runCount int32 = 10
+	var passCount int32 = 0
+	var mu sync.Mutex
 
-		err := cmd.Run()
-		if err != nil {
-			t.Fatal(err)
-		}
+	t.Parallel()
 
-		if !strings.Contains(out.String(), "2701") {
-			t.Fatalf("Expected '2701' in command output, got %s", out.String())
-		}
-	})
+	for i := 0; i < 3; i++ {
+		t.Run("Test command execution on '-y' argument", func(t *testing.T) {
+			t.Parallel()
+
+			cmd := exec.Command("./llamaspit", "-y", "multiply 37 by 73")
+			var out bytes.Buffer
+			cmd.Stdout = &out
+
+			err := cmd.Run()
+			mu.Lock()
+			runCount++
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if strings.Contains(out.String(), "2701") {
+				passCount++
+			}
+			mu.Unlock()
+		})
+	}
+
+	t.Logf("Total run: %v, Passes: %v, Pass rate: %v percent", runCount, passCount, (passCount/runCount)*100)
 }
